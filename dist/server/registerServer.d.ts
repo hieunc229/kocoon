7a731bfab1d4b9d9cf35531213d627ea062e816c
Hieu Nguyen (Jack) <nc.hieu@live.com>
2023-07-23 16:48:47 +0700

    Remove dist

--- dist/server/registerServer.d.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-export default function registerServer(options: {
-    app: Express;
-    debug?: boolean;
-    route: string;
-    location: string;
-}): Promise<void>;
